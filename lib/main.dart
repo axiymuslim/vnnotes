828cd125f938fafbe62246541c7385c0ae551f18
Axiy <Axiymuslim@gmail.com>
2023-01-22 23:09:54 +0500

    10:18:02

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,51 @@
-// ignore_for_file: avoid_print
+// ignore_for_file: avoid_print, unused_import, prefer_const_constructors
 
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import '../firebase_options.dart';
 import 'package:vnnotes/views/login_view.dart';
 import 'package:vnnotes/views/register_view.dart';
-import 'firebase_options.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(const MyApp());
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: "Flutter Demo",
+    theme: ThemeData(
+      primarySwatch: Colors.blue,
+    ),
+    home: HomePage(),
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Login"),
       ),
-      home: const RegisterView(),
+      body: FutureBuilder(
+        future: Firebase.initializeApp(
+          options: DefaultFirebaseOptions.currentPlatform,
+        ),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.done:
+              final user = FirebaseAuth.instance.currentUser;
+              if (user?.emailVerified ?? false) {
+                print('Verified');
+              } else {
+                print('Not verified');
+              }
+              return const Text('done');
+            default:
+              return const Text('loading');
+          }
+        },
+      ),
     );
   }
 }
