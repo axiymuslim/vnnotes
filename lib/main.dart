ccdede7cefa07355d693badadb7686da1045c482
Axiy <Axiymuslim@gmail.com>
2023-01-26 00:30:01 +0500

    22.09

--- lib/main.dart
+++ lib/main.dart
@@ -22,12 +22,8 @@ void main() {
       routes: {
         loginRoute: (context) => LoginView(),
         registerRoute: (context) => RegisterView(),
-<<<<<<< HEAD
         notesRoute: (context) => NotesView(),
         veriifyEmailRoute: (context) => VerifyEmailView()
-=======
-        notesRoute: (context) => NotesView()
->>>>>>> 87f639c207523860adfb321f403a8161d03ffe3f
       },
     ),
   );
