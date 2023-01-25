a442f2c9f7813bebe2c59d9f738a4017ce7d1270
Axiy <Axiymuslim@gmail.com>
2023-01-26 00:33:28 +0500

    22.09

--- lib/constants/routes.dart
+++ lib/constants/routes.dart
@@ -1,7 +1,4 @@
 const loginRoute = '/login/';
 const registerRoute = '/register/';
 const notesRoute = '/notes/';
-<<<<<<< HEAD
 const veriifyEmailRoute = '/verify-email/';
-=======
->>>>>>> 87f639c207523860adfb321f403a8161d03ffe3f
